45e5997f3bfe9cd4546e6dd1a45ba764dfd540e4
Roshan Krishnan <r.krishnan1@umiami.edu>
2014-06-28 17:37:13 -0500

    Initial Commit

---
+++ ProximityWahApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProximityWahApp
+//
+//  Created by Roshan Krishnan on 6/28/14.
+//  Copyright (c) 2014 Roshan Krishnan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ProximityWahApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ProximityWahApp
+//
+//  Created by Roshan Krishnan on 6/28/14.
+//  Copyright (c) 2014 Roshan Krishnan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
